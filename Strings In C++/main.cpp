04a47587013a5a7945d003616fd7d4acac81238c
themoffat28 <bthemoffat@gmail.com>
2017-02-02 08:44:49 -0700

    Initial Commit

---
+++ Strings In C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Strings In C++
+//
+//  Created by Moffat, Braeden on 2/2/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
